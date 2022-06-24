583ae5f347c44946e3cd2b31c1e13bfc07c6b53a
Pathikrit Syam <73285702+pathikritsyam@users.noreply.github.com>
2022-06-24 23:16:40 +0530

    Time: 16 ms (83.30%), Space: 22.3 MB (74.77%) - LeetHub

--- 337-house-robber-iii/337-house-robber-iii.cpp
+++ 337-house-robber-iii/337-house-robber-iii.cpp
@@ -11,17 +11,34 @@
  */
 class Solution {
 public:
-    int f(TreeNode* root,unordered_map<TreeNode*,int>&dp){
-        if(root==NULL)return 0;
-        if(dp.find(root)!=dp.end())return dp[root];
-        int including_root=root->val;
-        if(root->left)including_root+=f(root->left->left,dp)+f(root->left->right,dp);
-        if(root->right)including_root+=f(root->right->left,dp)+f(root->right->right,dp);
-        int excluding_root=f(root->left,dp)+f(root->right,dp);
-        return dp[root]=max(including_root,excluding_root);
-    }
+    // int f(TreeNode* root,unordered_map<TreeNode*,int>&dp){
+    //     if(root==NULL)return 0;
+    //     if(dp.find(root)!=dp.end())return dp[root];
+    //     int including_root=root->val;
+    //     if(root->left)including_root+=f(root->left->left,dp)+f(root->left->right,dp);
+    //     if(root->right)including_root+=f(root->right->left,dp)+f(root->right->right,dp);
+    //     int excluding_root=f(root->left,dp)+f(root->right,dp);
+    //     return dp[root]=max(including_root,excluding_root);
+    // }
     int rob(TreeNode* root) {
-        unordered_map<TreeNode*,int>dp;
-        return f(root,dp);
+    //     unordered_map<TreeNode*,int>dp;
+    //     return f(root,dp);
+    //     dp[NULL]=0;
+    //     while(root){
+    //         int including_root=root->val;
+    //         if(root->left)including_root+=dp[root->left->left]+dp[root->left->right];
+    //         if(root->right)including_root+=dp[root->right->left]+dp[root->right->right];
+    //         int excluding_root=dp[root->left]+dp[root->right];
+    //     }
+    //     return dp[root]
+        //*SPACE OPTIMISED CODE*
+        auto ans=f(root);
+        return max(ans.first,ans.second);
+    }
+    pair<int,int> f(TreeNode* root){
+        if(root==NULL)return {0,0};
+        auto [leftdontrob,leftrob]=f(root->left);
+        auto [rightdontrob,rightrob]=f(root->right);
+        return {max(leftdontrob,leftrob)+max(rightdontrob,rightrob),root->val+leftdontrob+rightdontrob};
     }
 };
