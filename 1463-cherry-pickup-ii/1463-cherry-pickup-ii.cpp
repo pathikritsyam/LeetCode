4ae95a05de4bd4c71e700cc2525d80525dead3cf
Pathikrit Syam <73285702+pathikritsyam@users.noreply.github.com>
2022-06-24 09:25:11 +0530

    Time: 209 ms (18.29%), Space: 14.4 MB (49.88%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,25 +1,35 @@
+//Tabulation
 class Solution {
 public:
     int cherryPickup(vector<vector<int>>& grid) {
         int n=grid.size(),m=grid[0].size();        
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(m,-1)));
-        return f(0,0,m-1,grid,n,m,dp);
-    }
-    int f(int i,int j1,int j2,vector<vector<int>>&grid,int n,int m,vector<vector<vector<int>>>&dp){
-        if(j1<0||j1>m-1||j2<0||j2>m-1)return -1e8;
-        if(dp[i][j1][j2]!=-1)return dp[i][j1][j2];
-        if(i==n-1){
-            if(j1==j2)return grid[i][j1];
-            else return grid[i][j1]+grid[i][j2];
-        }
-        int dj[]={-1,0,1};
-        int maxi=0;
-        for(int dj1:dj){
-            for(int dj2:dj){
-                if(j1==j2) maxi=max(maxi,grid[i][j1]+f(i+1,j1+dj1,j2+dj2,grid,n,m,dp));
-                else maxi=max(maxi,grid[i][j2]+grid[i][j1]+f(i+1,j1+dj1,j2+dj2,grid,n,m,dp));
+        vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(m,0)));
+        for(int j1=0;j1<=m-1;j1++){
+            for(int j2=0;j2<=m-1;j2++){
+                if(j1==j2) dp[n-1][j1][j2]=grid[n-1][j1];
+                else dp[n-1][j1][j2]=grid[n-1][j1]+grid[n-1][j2];
             }
         }
-        return dp[i][j1][j2]=maxi;
+        for(int i=n-2;i>=0;i--){
+            for(int j1=0;j1<m;j1++){
+                for(int j2=0;j2<m;j2++){
+                    int dj[]={-1,0,1};
+                    int maxi=-1e8;
+                    for(int dj1:dj){
+                        for(int dj2:dj){
+                            if(j1+dj1>=0&&j1+dj1<m&&j2+dj2>=0&&j2+dj2<m){
+                                if(j1==j2) maxi=max(maxi,grid[i][j1]+dp[i+1][j1+dj1][j2+dj2]);
+                                else maxi=max(maxi,grid[i][j2]+grid[i][j1]+dp[i+1][j1+dj1][j2+dj2]);
+            }
+                            // else{
+                            //     maxi=max(maxi,-1e9);
+                            // }
+                        }
+        }
+                    dp[i][j1][j2]=maxi;
+                }
+            }
+        }
+        return dp[0][0][m-1];
     }
 };
