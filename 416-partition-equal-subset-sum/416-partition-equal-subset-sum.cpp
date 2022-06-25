6f753c55f1ff18982770193b73f06926e13d3093
Pathikrit Syam <73285702+pathikritsyam@users.noreply.github.com>
2022-06-25 21:46:56 +0530

    Time: 516 ms (18.44%), Space: 12 MB (59.35%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -4,16 +4,18 @@ public:
         int sum=0,n=nums.size();
         for(int i=0;i<nums.size();i++)sum+=nums[i];
         if(sum&1)return false;
-        vector<vector<int>>dp(n,vector<int>(sum/2+1,-1));
-        return f(n-1,sum/2,nums,dp);
-    }
-    bool f(int n,int target,vector<int>&nums,vector<vector<int>>&dp){
-        if(target==0)return true;
-        if(n==0) return (target==nums[n]);
-        if(dp[n][target]!=-1)return dp[n][target];
-        bool dont_take=f(n-1,target,nums,dp);
-        bool take=false;
-        if(nums[n]<=target)take=f(n-1,target-nums[n],nums,dp);
-        return dp[n][target]=take+dont_take;
+        int target=sum/2;
+        vector<vector<bool>>dp(n,vector<bool>(sum/2+1,false));
+        for(int i=0;i<n;i++)dp[i][0]=true;
+        if(nums[0]<=target)dp[0][nums[0]];
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=target;j++){
+                bool dont_take=dp[i-1][j];
+                bool take=false;
+                if(nums[i]<=j)take=dp[i-1][j-nums[i]];
+                dp[i][j]=take+dont_take;
+            }
+        }
+        return dp[n-1][sum/2];
     }
 };
