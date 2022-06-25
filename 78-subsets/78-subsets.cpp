02d0314a325301fa605d9d01ed5891b73d548f13
Pathikrit Syam <73285702+pathikritsyam@users.noreply.github.com>
2022-06-25 16:30:17 +0530

    Time: 6 ms (36.75%), Space: 7.2 MB (69.84%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,21 +1,36 @@
+// class Solution {
+// public:
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//         vector<int>ds;
+//         vector<vector<int>>ans;
+//         f(0,ds,nums,ans);
+//         return ans;
+//     }
+//     void f(int i,vector<int>&ds,vector<int>&nums,vector<vector<int>>&ans){
+//         if(i==nums.size()){
+//             ans.push_back(ds);
+//             return;
+//         }
+//         //Taking the index
+//         ds.push_back(nums[i]);
+//         f(i+1,ds,nums,ans);
+//         //Not taking the index
+//         ds.pop_back();
+//         f(i+1,ds,nums,ans);
+//     }
+// };
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int>ds;
+        int n=nums.size();
         vector<vector<int>>ans;
-        f(0,ds,nums,ans);
-        return ans;
-    }
-    void f(int i,vector<int>&ds,vector<int>&nums,vector<vector<int>>&ans){
-        if(i==nums.size()){
-            ans.push_back(ds);
-            return;
+        for(int i=0;i<(1<<n);i++){
+            vector<int>sub;
+            for(int j=0;j<n;j++){
+                if(i&(1<<j))sub.push_back(nums[j]);
+            }
+            ans.push_back(sub);
         }
-        //Taking the index
-        ds.push_back(nums[i]);
-        f(i+1,ds,nums,ans);
-        //Not taking the index
-        ds.pop_back();
-        f(i+1,ds,nums,ans);
+        return ans;
     }
 };
