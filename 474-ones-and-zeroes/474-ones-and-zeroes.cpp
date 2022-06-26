bc1d787047e6cf2cd5cbc59ab62072a10471771f
Pathikrit Syam <73285702+pathikritsyam@users.noreply.github.com>
2022-06-26 19:55:01 +0530

    Time: 644 ms (18.37%), Space: 102.4 MB (12.07%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -4,24 +4,26 @@ public:
         int l=strs.size();
         int zero=count(strs[0].begin(),strs[0].end(),'0');
         int ones=count(strs[0].begin(),strs[0].end(),'1');
-        vector<vector<vector<int>>>dp(l,vector<vector<int>>(m+1,vector<int>(n+1,0)));
+        vector<vector<int>>prev(m+1,vector<int>(n+1,0));
         for(int i=zero;i<=m;i++){
             for(int j=ones;j<=n;j++){
-                dp[0][i][j]=1;
+                prev[i][j]=1;
             }
         }
         for(int i=1;i<l;i++){
             int zero=count(strs[i].begin(),strs[i].end(),'0');
             int ones=count(strs[i].begin(),strs[i].end(),'1');
+            vector<vector<int>>curr(m+1,vector<int>(n+1,0));
             for(int j=0;j<=m;j++){
                 for(int k=0;k<=n;k++){
-                    int dont_take=0+dp[i-1][j][k];
+                    int dont_take=0+prev[j][k];
                     int take=0;
-                    if((j>=zero)&&(k>=ones))take=1+dp[i-1][j-zero][k-ones];
-                    dp[i][j][k]=max(take,dont_take); 
+                    if((j>=zero)&&(k>=ones))take=1+prev[j-zero][k-ones];
+                    curr[j][k]=max(take,dont_take); 
                 }
             }
+            prev=curr;
         }
-        return dp[l-1][m][n];
+        return prev[m][n];
     }
 };
