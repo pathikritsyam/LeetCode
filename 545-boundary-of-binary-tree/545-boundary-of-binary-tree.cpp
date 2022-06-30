78206ec6cd4395eb6804648833cd750f1aba2d20
Pathikrit Syam <73285702+pathikritsyam@users.noreply.github.com>
2022-07-01 00:49:49 +0530

    Time: 15 ms (56.25%), Space: 20.8 MB (96.88%) - LeetHub

---
+++ 545-boundary-of-binary-tree/545-boundary-of-binary-tree.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isleaf(TreeNode* root){
+            return !root->left && !root->right;
+    }
+    vector<int> boundaryOfBinaryTree(TreeNode* root) {
+        vector<int>res;
+        if(root==NULL)return res;
+        if(!isleaf(root))res.push_back(root->val);
+        addLeftBoundary(root,res);
+        addLeaves(root,res);
+        addRightBoundary(root,res);
+        return res;
+    }
+    void addLeftBoundary(TreeNode* root,vector<int>&res){
+        TreeNode* cur=root->left;
+        while(cur){
+            if(!isleaf(cur))res.push_back(cur->val);
+            if(cur->left)cur=cur->left;
+            else cur=cur->right;
+        }
+    }
+    void addRightBoundary(TreeNode* root,vector<int>&res){
+        vector<int>tmp;
+        TreeNode* cur=root->right;
+        while(cur){
+            if(!isleaf(cur))tmp.push_back(cur->val);
+            if(cur->right)cur=cur->right;
+            else cur=cur->left;
+        }
+        for(int i=tmp.size()-1;i>=0;i--)res.push_back(tmp[i]);
+    }
+    void addLeaves(TreeNode* root,vector<int>&res){
+        if(isleaf(root)){
+            res.push_back(root->val);
+            return;
+        }
+        if(root->left)addLeaves(root->left,res);
+        if(root->right)addLeaves(root->right,res);
+    }
+};
