b7e24dfda0bcfd54dc0d38b687f2af18bb6a6eb5
Pathikrit Syam <73285702+pathikritsyam@users.noreply.github.com>
2022-07-11 08:52:52 +0530

    Time: 340 ms (84.13%), Space: 141.5 MB (33.81%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,14 +1,40 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int>dq;
-        vector<int>res;
-        for(int i=0;i<nums.size();i++){
-            if(!dq.empty()&&dq.front()==(i-k))dq.pop_front();
-            while(!dq.empty()&&nums[dq.back()]<nums[i])dq.pop_back();
-            dq.push_back(i);
-            if(i>=k-1)res.push_back(nums[dq.front()]);
+        int n=nums.size();
+        vector<int>lft(n),rgt(n),res;
+        lft[0]=nums[0];
+        for(int i=1;i<n;i++){
+            lft[i]=nums[i];
+            if(i%k!=0){
+                lft[i]=max(lft[i-1],lft[i]);
+            }
+        }
+        rgt[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            rgt[i]=nums[i];
+            if(i%k!=k-1){
+                rgt[i]=max(rgt[i+1],rgt[i]);
+            }
+        }
+        for(int i=0;i<n-k+1;i++){
+            res.push_back(max(rgt[i],lft[i+k-1]));
         }
         return res;
     }
 };
+//Soln Using Dequeue
+// class Solution {
+// public:
+//     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+//         deque<int>dq;
+//         vector<int>res;
+//         for(int i=0;i<nums.size();i++){
+//             if(!dq.empty()&&dq.front()==(i-k))dq.pop_front();
+//             while(!dq.empty()&&nums[dq.back()]<nums[i])dq.pop_back();
+//             dq.push_back(i);
+//             if(i>=k-1)res.push_back(nums[dq.front()]);
+//         }
+//         return res;
+//     }
+// };
