453986336be89eab9bd58cfaa824c78f2abf08ba
Pathikrit Syam <73285702+pathikritsyam@users.noreply.github.com>
2022-07-12 14:30:53 +0530

    Time: 19 ms (46.39%), Space: 8.7 MB (44.91%) - LeetHub

--- 877-stone-game/877-stone-game.cpp
+++ 877-stone-game/877-stone-game.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
+    int dp[501][501];
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int l=0,r=piles.size()-1,sum=0;
+        memset(dp,-1,sizeof(dp));
+        for(int i=0;i<=r;i++)sum+=piles[i];
+        int alice=f(l,r,piles);
+        return alice>(sum-alice);
+    }
+    int f(int l,int r,vector<int>&piles){
+        if(l>=r)return 0;
+        if(dp[l][r]!=-1)return dp[l][r];
+        int left=piles[l]+min(f(l+2,r,piles),f(l+1,r-1,piles));
+        int right=piles[r]+min(f(l,r-2,piles),f(l+1,r-1,piles));
+        return dp[l][r]=max(left,right);
     }
 };
