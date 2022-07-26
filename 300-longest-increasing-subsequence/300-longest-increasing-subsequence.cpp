da76f3e98da264e384e4ee81cd2d3424d4171f9e
Pathikrit Syam <73285702+pathikritsyam@users.noreply.github.com>
2022-07-26 19:11:08 +0530

    Time: 1139 ms (10.61%), Space: 292.2 MB (6.79%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -16,16 +16,4 @@ public:
         }
         return dp[0][0];
     }
-    int f(int i,int j,vector<int>&nums,vector<vector<int>>&dp){
-        int n=nums.size();
-        if(i==n)return 0;
-        if(dp[i][j+1]!=-1)return dp[i][j+1];
-        int not_take=f(i+1,j,nums,dp);
-        int take=0;
-        if(j==-1||nums[i]>nums[j]){
-            take=1+f(i+1,i,nums,dp);
-        }
-        int len=max(take,not_take);
-        return dp[i][j+1]=len;
-    }
 };
