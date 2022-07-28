c40e27a93ed765e998924f3d6bc0b0683c830c30
Pathikrit Syam <73285702+pathikritsyam@users.noreply.github.com>
2022-07-28 13:53:39 +0530

    Time: 4 ms (64.36%), Space: 6.7 MB (74.84%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
-        int mp[26];
-        unordered_map<char,int> in_deque_mp;
+        int mp[26],dq[26];
+        memset(dq,0,sizeof(dq));
         deque<int> q;
         int n = s.length();
         for(int i = 0; i<n; i++) mp[s[i]-'a'] = i;
         for(int i = 0; i<n; i++) { 
             char c = s[i];
-            if(in_deque_mp[c]==0) {
+            if(dq[c-'a']==0) {
                 while(!q.empty() && c<q.back() && mp[q.back()-'a']>i) {
-                    in_deque_mp[q.back()]=0;
+                    dq[q.back()-'a']=0;
                     q.pop_back();
                 }
-                in_deque_mp[c] = 1;
+                dq[c-'a'] = 1;
                 q.push_back(c);
             }
         }
