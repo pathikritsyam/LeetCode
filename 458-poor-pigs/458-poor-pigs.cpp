ccf163c11f7e294c5caf5fff004e11502ccd29eb
Pathikrit Syam <73285702+pathikritsyam@users.noreply.github.com>
2022-08-06 15:23:29 +0530

    Time: 4 ms (25.82%), Space: 6.1 MB (58.91%) - LeetHub

---
+++ 458-poor-pigs/458-poor-pigs.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        int states=minutesToTest/minutesToDie+1;
+        return ceil(log(buckets)/log(states));
+    }
+};
+//
