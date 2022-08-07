b9220a326ead1db65a833fb00f1259bd2dd367c9
Pathikrit Syam <73285702+pathikritsyam@users.noreply.github.com>
2022-08-07 16:36:48 +0530

    Time: 480 ms (74.55%), Space: 10.4 MB (18.67%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.cpp
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -4,18 +4,19 @@ public:
         int n=nums.size();
         nums.push_back(1);
         nums.insert(nums.begin(),1);
-        vector<vector<int>>dp(n+2,vector<int>(n+2,-1));
-        return f(1,n,nums,dp);
-    }
-    int f(int i,int j,vector<int>&nums,vector<vector<int>>&dp){
-        if(i>j)return 0;
-        int maxi=-1e9;
-        if(dp[i][j]!=-1)return dp[i][j];
-        for(int k=i;k<=j;k++){
-            int points=nums[i-1]*nums[k]*nums[j+1]+f(i,k-1,nums,dp)+f(k+1,j,nums,dp);
-            maxi=max(maxi,points);
+        vector<vector<int>>dp(n+2,vector<int>(n+2,0));
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n;j++){
+                if(i>j)continue;
+                int maxi=-1e9;
+                for(int k=i;k<=j;k++){
+                    int points=nums[i-1]*nums[k]*nums[j+1]+dp[i][k-1]+dp[k+1][j];
+                    maxi=max(maxi,points);
+                }
+                dp[i][j]=maxi;
+            }
         }
-        return dp[i][j]=maxi;
+        return dp[1][n];
     }
 };
 //1 3 1 5 8 1
