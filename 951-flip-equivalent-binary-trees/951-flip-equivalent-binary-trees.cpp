502f5513a0a4e354b414cb4469d9aeed6d89b458
Pathikrit Syam <73285702+pathikritsyam@users.noreply.github.com>
2022-08-09 15:06:12 +0530

    Time: 15 ms (5.22%), Space: 12.3 MB (68.72%) - LeetHub

--- 951-flip-equivalent-binary-trees/951-flip-equivalent-binary-trees.cpp
+++ 951-flip-equivalent-binary-trees/951-flip-equivalent-binary-trees.cpp
@@ -15,7 +15,7 @@ public:
         return dfs(root1,root2);
     }
     bool dfs(TreeNode* root1, TreeNode* root2){
-        if(root1==root2)return true;
+        if(root1==NULL&&root2==NULL)return true;
         if(!root1||!root2||root1->val!=root2->val)return false;
         return (dfs(root1->left,root2->right)&&dfs(root1->right,root2->left))||(dfs(root1->left,root2->left)&&dfs(root1->right,root2->right));
     }
