e7d7a78e36369e57da88ded04af95a6c258c3c64
Pathikrit Syam <73285702+pathikritsyam@users.noreply.github.com>
2022-11-06 10:05:01 +0530

    Time: 2936 ms (5.04%), Space: 15.2 MB (43.50%) - LeetHub

--- 212-word-search-ii/212-word-search-ii.cpp
+++ 212-word-search-ii/212-word-search-ii.cpp
@@ -1,6 +1,6 @@
 struct TrieNode {
     TrieNode* children[26] = {};
-    string* word;
+    bool endsHere=false;
     void addWord(string& word) {
         TrieNode* cur = this;
         for (char c : word) {
@@ -8,10 +8,9 @@ struct TrieNode {
             if (cur->children[c] == nullptr) cur->children[c] = new TrieNode();
             cur = cur->children[c];
         }
-        cur->word = &word;
+        cur->endsHere = true;
     }
 };
-
 class Solution {
 public:
     int m, n;
@@ -24,19 +23,23 @@ public:
         
         for (int r = 0; r < m; ++r)
             for (int c = 0; c < n; ++c)
-                dfs(board, r, c, &trieNode);
+                dfs(board, r, c, &trieNode,"");
         return ans;
     }
-    void dfs(vector<vector<char>>& board, int r, int c, TrieNode* cur) {
+    void dfs(vector<vector<char>>& board, int r, int c, TrieNode* cur,string res) {
         if (r < 0 || r == m || c < 0 || c == n || board[r][c] == '#' || cur->children[board[r][c]-'a'] == nullptr) return;
         char orgChar = board[r][c];
         cur = cur->children[orgChar - 'a'];
-        if (cur->word != nullptr) {
-            ans.push_back(*cur->word);
-            cur->word = nullptr; // Avoid duplication!
+        if (cur->endsHere!=false) {
+            res.push_back(board[r][c]);
+            ans.push_back(res);
+            res.pop_back();
+            cur->endsHere = false; // Avoid duplication!
         }
+        res.push_back(board[r][c]);
         board[r][c] = '#'; // mark as visited!
-        for (int i = 0; i < 4; ++i) dfs(board, r + DIR[i], c + DIR[i+1], cur);
-        board[r][c] = orgChar; // restore org state
+        for (int i = 0; i < 4; ++i) dfs(board, r + DIR[i], c + DIR[i+1], cur,res);
+        board[r][c] = orgChar;
+        res.pop_back();// restore org state
     }
 };
